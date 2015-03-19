c74982ab17dcd74c063a690a57aa53b9772a0b75
Yashprit Singh <yashprits@gmail.com>
2015-03-19 23:25:48 +0530

    intial setup, getting hadn dirty on go lang

--- colorlog.go
+++ colorlog.go
@@ -1,6 +1,9 @@
-package logger
+package log
+
+//learning:: if you need to export method, start it with capital letter
 
 import "fmt"
+import "github.com/mgutz/ansi"
 
 /**
  * Enable debugging
@@ -8,7 +11,54 @@ import "fmt"
  */
 var isDebug = false
 
-func info(key string, args ...interface{}) {
-	p := fmt.Sprintf(key, args)
-	fmt.Println(p)
+/**
+ * Internal method to make code dry
+ * @param {string} color string       color of string
+ * @param {string} key   string       message with placeholder
+ * @param {interface} args interface  veriadic params
+ */
+func colorIt(color string, key string, args ...interface{}) {
+	p := fmt.Sprintf(key, args...)
+	msg := ansi.Color(p, color)
+	fmt.Println(msg)
+}
+
+/**
+ * Info method
+ *
+ * @param {string} key   string       message with placeholder
+ * @param {interface} args interface  veriadic params
+ */
+func Info(key string, args ...interface{}) {
+	colorIt("green", key, args...)
+}
+
+/**
+ * Debug method
+ *
+ * @param {string} key   string       message with placeholder
+ * @param {interface} args interface  veriadic params
+ */
+func Debug(key string, args ...interface{}) {
+	colorIt("blue", key, args...)
+}
+
+/**
+ * Warn method
+ *
+ * @param {string} key   string       message with placeholder
+ * @param {interface} args interface  veriadic params
+ */
+func Warn(key string, args ...interface{}) {
+	colorIt("yellow", key, args...)
+}
+
+/**
+ * Error method
+ *
+ * @param {string} key   string       message with placeholder
+ * @param {interface} args interface  veriadic params
+ */
+func Error(key string, args ...interface{}) {
+	colorIt("red", key, args...)
 }
--- example/main.go
+++ example/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"github.com/yashprit/go-cli-colorize-logger"
 )
 
 func main() {
-	log.info("Hello %s", "hi")
+	log.Info("Hello %s %d", "hi", 1)
+	log.Debug("This is debug %f", 1.0)
+	log.Warn("This will work, but please check %s", "not a register user")
+	log.Error("%s", errors.New("Something wents wrong"))
 }
